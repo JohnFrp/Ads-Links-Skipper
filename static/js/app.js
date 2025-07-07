4c8c0ff695b765c311e0fd52ff4b26c8f5f7983c
JohnFrp <125874717+JohnFrp@users.noreply.github.com>
2025-07-07 16:15:31 +0630

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -15,17 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
             if (linksContainer) {
                 linksContainer.innerHTML = '';
                 
-                // Re-add the heading if it was removed
-                if (!linksContainer.querySelector('h3')) {
-                    const heading = document.createElement('h3');
-                    heading.innerHTML = `
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="#00d4ff">
-                            <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/>
-                        </svg>
-                        
-                    `;
-                    linksContainer.appendChild(heading);
-                }
+       
             }
             
             // Clear any error messages
